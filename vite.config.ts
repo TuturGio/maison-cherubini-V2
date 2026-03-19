4d951a9556d9dc9460c91f21204119711ad3cf85
Arthur G <83343872+TuturGio@users.noreply.github.com>
2026-03-19 11:58:37 +0100

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/maison-cherubini-V2/' : '/',
+  base: '/maison-cherubini-V2/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
