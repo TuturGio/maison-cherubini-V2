d44c56a93d2bfda5edb56b245b5af003feea418e
Arthur G <83343872+TuturGio@users.noreply.github.com>
2026-07-08 17:17:34 +0200

    Updated Showroom.tsx

--- src/pages/Showroom.tsx
+++ src/pages/Showroom.tsx
@@ -97,7 +97,7 @@ export default function Showroom() {
           <div className="relative min-h-[400px] md:min-h-0">
             <iframe
               title="Localisation Nuance Décoration"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2903.5!2d5.3705!3d43.2965!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12c9c0b7c4e7b5b5%3A0x0!2s15%20Rue%20de%20la%20R%C3%A9publique%2C%2013001%20Marseille!5e0!3m2!1sfr!2sfr!4v1699000000000!5m2!1sfr!2sfr"
+              src="https://maps.google.com/maps?q=63+Av.+de+Montredon,+13008+Marseille,+France&output=embed"
               className="absolute inset-0 w-full h-full border-0"
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
