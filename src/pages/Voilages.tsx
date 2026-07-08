ec591956239e1ef00a7db2de3cebabd242a3a530
Arthur G <83343872+TuturGio@users.noreply.github.com>
2026-07-08 17:08:10 +0200

    Updated Voilages.tsx

--- src/pages/Voilages.tsx
+++ src/pages/Voilages.tsx
@@ -15,7 +15,7 @@ export default function Voilages() {
         'Finitions soignées et discrètes',
         'Entretien facile et durable',
       ]}
-      heroImage={`${import.meta.env.BASE_URL}voilage-cat.jpg`}
+      heroImage={`${import.meta.env.BASE_URL}voilage-cat copy.jpg`}
       heroDraw={drawSheer}
       galleryDraws={[
         drawSheer,
