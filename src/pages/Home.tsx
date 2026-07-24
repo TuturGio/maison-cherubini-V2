2d1a46b99e8e9818cf636597ca71160457daf44d
Arthur G <83343872+TuturGio@users.noreply.github.com>
2026-07-24 15:56:45 +0200

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -188,7 +188,7 @@ export default function Home() {
       link: '/realisations/banquettes',
     },
     {
-      title: 'Revêtements extérieurs',
+      title: 'Outdoor',
       subtitle: 'Banquettes, coussins & transats',
       image: `${import.meta.env.BASE_URL}outdoor-home.jpg`,
       link: '/realisations/bateaux',
